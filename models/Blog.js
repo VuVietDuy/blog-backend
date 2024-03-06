ab50f79ca4d5ab18cb61ac734078a0f1a355ea73
VuVietDuy <vuvietduy1234@gmail.com>
2024-03-06 14:02:17 +0700

    Update

--- models/Blog.js
+++ models/Blog.js
@@ -3,9 +3,10 @@ const Schema = mongoose.Schema;
 
 const Blog = new Schema({
     title: { type: String},
+    description: {type: String},
     content: { type: String},
     tag: [String],
-    createdBy: { type: mongoose.Schema.Types.ObjectId, ref:'User', required: true },
+    // createdBy: { type: mongoose.Schema.Types.ObjectId, ref:'User', required: true },
     createdAt: { type: Date, default: Date.now },
     updatedAt: { type: Date, default: Date.now },
 })
--- routes/questions.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var questionsController = require('../controller/question.controller')
-
-router.get('', questionsController.getAllQuestion)
-router.post('', questionsController.createQuestion)
-
-module.exports = router;
